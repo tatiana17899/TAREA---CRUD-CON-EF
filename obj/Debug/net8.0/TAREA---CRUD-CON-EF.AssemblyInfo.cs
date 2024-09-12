374036d219f5ff70b0fec18bcf3e2d6fa5c2103b
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-09-12 17:48:13 -0500

    aaa

--- obj/Debug/net8.0/TAREA---CRUD-CON-EF.AssemblyInfo.cs
+++ obj/Debug/net8.0/TAREA---CRUD-CON-EF.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TAREA---CRUD-CON-EF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408131e8a2304eb9c5a46186e9c7e985a2d8df87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de0f3f03ad4fa5abb31948c3289404fd9c7368e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TAREA---CRUD-CON-EF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TAREA---CRUD-CON-EF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
