5ea296f5059889b15f197caaba4d2cd4a196c1f8
tatiana17899 <tatianasuarezrosas332@gmail.com>
2024-09-12 17:10:28 -0500

    Crear mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TAREA___CRUD_CON_EF.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
